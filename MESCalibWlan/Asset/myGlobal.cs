8ebc0b56d8bce1132b50c29d8df01b788094428b
hoducanh1988 <hoducanh1988@gmail.com>
2021-10-26 17:05:40 +0700

    id:0x06

---
+++ MESCalibWlan/Asset/myGlobal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESCalibWlan.Asset {
+    public class myGlobal {
+        public static string settingFileFullName = string.Format("{0}setting.xml", AppDomain.CurrentDomain.BaseDirectory);
+        public static SettingInformation mySetting = new SettingInformation();
+
+    }
+}
